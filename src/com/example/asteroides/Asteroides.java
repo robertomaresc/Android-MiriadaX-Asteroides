34324ec4026f4e0b0d6fa22eff75c447b9391a5d
Roberto Martin Escribano <roberto.martes@gmail.com>
2013-04-10 00:17:03 +0200

    Funcionalidad Modulo 9: - Ejercicio paso a paso: Almacenando la ltima puntuacin en un fichero de preferencias. (OK)

--- src/com/example/asteroides/Asteroides.java
+++ src/com/example/asteroides/Asteroides.java
@@ -102,9 +102,9 @@ public class Asteroides extends Activity {
 
 	private AlmacenPuntuaciones crearAlmacenPuntuaciones() {
 		AlmacenPuntuaciones almacen = null;
-		almacen = new AlmacenPuntuacionesArrayImpl();
+		// almacen = new AlmacenPuntuacionesArrayImpl();
 		almacen = new AlmacenPuntuacionesPreferencesImpl(this);
-		almacen = new AlmacenPuntuacionesFicheroInternoImpl(this);
+		// almacen = new AlmacenPuntuacionesFicheroInternoImpl(this);
 		return almacen;
 	}
 
